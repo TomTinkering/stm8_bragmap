7cd83a55daa3bbfca2dcad5201c8cabc3391f34c
Tom Vocke <github@vocke.tv>
2015-02-13 19:20:16 +0100

    added datasheets, started work on chip include file (stm8s003.h), included pin mapping for design in comments in main

--- main.c
+++ main.c
@@ -4,8 +4,66 @@
  *  Created on: Feb 12, 2015
  *      Author: tomv
  */
-#include "stm8l.h"
+#include "stm8s_custom.h"
 #include "blink.h"
+
+/*
+ *
+ * pinout stm8s
+
+input / output cap touch pairs
+
+pd2 / pd3
+pa1 / pa2
+pb4 / pb5
+pc3 / pc4
+
+uart info / debugging
+
+pd5
+pd6
+
+master interface (SPI)
+
+pc5 / pc6 / pc7
+
+toggle measurement
+
+pa3
+ *
+ *
+ *
+ */
+
+/*
+ * master sets control pin high
+ * masters waits Xms, satelites perform measurement
+ * master sets control pin low (satelites store results)
+ * master retreives results
+ * repeat
+ *
+ */
+
+typedef enum ret_t {
+
+	EXIT_OK,
+
+
+} ret_t;
+
+
+int GPIO_touch_cycle(void)
+{
+
+    //set PORTA
+    PA_DDR = 0b00000000;
+    PA_ODR = 0b00000000;
+    PE_CR1 = 0b00000000;
+
+
+
+}
+
 
 int main() {
 
